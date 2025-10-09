0563207986fbf33523e5c958bc201815155011b6
azterbaez <rivasmingito48@gmail.com>
2025-10-09 09:23:16 -0600

    Components mensaje y titulo Agregados

---
+++ src/components/Mensaje.jsx
@@ -0,0 +1,14 @@
+import React from "react";  
+
+const Mensaje = () => {
+    return (
+        <>  
+            <h1>Programar con react es atroz</h1>
+          <ul>
+            <li>Es Facil</li>
+            <li>Porque copio y pego</li>
+            <li>chatgpt lo facilita</li>
+          </ul>
+        </>
+    );
+}
---
+++ src/components/Titulo.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+const Titulo = () => {
+    return (
+        <>
+            <h1>HOOLA GENTE </h1>
+            <h2>Estoy copiando codigos en React con Vite</h2>
+            <h3>Ayuda  a no estresarse</h3>
+            </>
+    );
+
+}
+export default Titulo;
