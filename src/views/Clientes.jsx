e06b6fcfffb3cc72baadc8bc519bf25b1d88d9d8
azterbaez <rivasmingito48@gmail.com>
2025-10-23 11:28:46 -0600

    Busqueda de Cliente Realizado

--- src/views/Clientes.jsx
+++ src/views/Clientes.jsx
@@ -1,22 +1,28 @@
-import { useEffect, useState } from "react";
-import { Container } from "react-bootstrap";
-import TablaClientes from "../components/clientes/TablaClientes";
+import {useState,useEffect} from 'react';
+import TablaClientes from '../components/clientes/TablaClientes';
+import { Container, Col, Row, Button } from 'react-bootstrap';
+import CuadroBusquedas from "../components/busquedas/CuadroBusquedas";
+
 
 const Clientes = () => {
-
     const [clientes, setClientes] = useState([]);
     const [cargando, setCargando] = useState(true);
 
+    const [clientesFiltrados, setClientesFiltrados] = useState([]);
+    const [textoBusqueda, setTextoBusqueda] = useState("");
+
+
     const obtenerClientes = async () => {
         try {
-            const respuesta = await fetch("http://localhost:3000/api/Clientes");
+            const respuesta = await fetch("http://localhost:3000/api/clientes");
 
             if (!respuesta.ok) {
-                throw new Error("Error al obtener las clientes");
+              throw new Error("Error al obtener los clientes");
             }
             const datos = await respuesta.json();
-
+            
             setClientes(datos);
+            setClientesFiltrados(datos);
             setCargando(false);
         } catch (error) {
             console.log(error.message);
@@ -24,20 +30,44 @@ const Clientes = () => {
         }
     }
 
+
+    const manejarCambioBusqueda = (e) => {
+        const texto = e.target.value.toLowerCase();
+        setTextoBusqueda(texto);
+
+        const filtrados = clientes.filter(
+            (clientes) =>
+                clientes.primer_nombre.toLowerCase().includes(texto) ||
+                clientes.id_cliente.toString().includes(texto)
+
+        );
+        setClientesFiltrados(filtrados);
+    };
+
+
     useEffect(() => {
         obtenerClientes();
     }, []);
+
     return (
         <>
             <Container className="mt-4">
                 <h4>Clientes</h4>
+                <Row>
+                    <Col lg={5} md={6} sm={8} xs={7}>
+                        <CuadroBusquedas
+                            textoBusqueda={textoBusqueda}
+                            manejarCambioBusqueda={manejarCambioBusqueda}
+                        />
+                    </Col>
+                </Row>
                 <TablaClientes
-                    clientes={clientes}
+                    clientes={clientesFiltrados}
                     cargando={cargando} />
             </Container>
         </>
     );
-    
 }
+
 
 export default Clientes;
